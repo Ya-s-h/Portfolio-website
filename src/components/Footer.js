c849cc213c63f91de15b0c82b552a477f253d248
Yash Aggarwal <vasu2013agg@gmail.com>
2023-08-14 20:20:26 +0530

    minor changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,12 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+  // let date = new Date();
+  // let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
-        </Col>
-        <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
@@ -31,6 +25,13 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
+          </ul>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Thanks for visiting</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
             {/* <li className="social-icons">
               <a
                 href="https://twitter.com/Soumyajit4419"

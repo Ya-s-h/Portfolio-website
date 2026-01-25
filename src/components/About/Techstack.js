946e6e454cbe6b251fb244c8807cb4ace4340aa7
Yash Aggarwal <vasu2013agg@gmail.com>
2026-01-25 17:54:14 +0530

    Updated Tech and tool stack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,68 +1,45 @@
-import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiLinux,
-} from "react-icons/di";
-import {
-  SiShell,
-  SiRedis,
-  SiExpress,
-  SiMysql,
-  SiSqlite,
-  SiFlask,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import Python from "../../Assets/TechIcons/python.svg";
+import bash from "../../Assets/TechIcons/bash.svg";
+import celery from "../../Assets/TechIcons/celery.svg";
+import es from "../../Assets/TechIcons/elasticsearch.svg";
+import git from "../../Assets/TechIcons/git.svg";
+import langchain from "../../Assets/TechIcons/langchain.svg";
+import mongo from "../../Assets/TechIcons/mongodb.svg";
+import sql from "../../Assets/TechIcons/mysql.svg";
+import redis from "../../Assets/TechIcons/redis.svg";
+
+
+function makeCol() {
+  const images_with_names = {
+    // Languages
+    Python: Python,
+    Bash: bash,
+    // DB
+    "Elastic Search": es,
+    "Mongo DB": mongo,
+    Redis: redis,
+    MySQL: sql,
+    // Tools
+    Git: git,
+    Celery: celery,
+    Langchain: langchain,
+    // Postman: postman,
+  };
+
+  return Object.entries(images_with_names).map(([img_name, image]) => (
+    <Col key={img_name} xs={4} md={2} className="tech-icons">
+      <img src={image} alt={img_name} height={30} />
+      <div className="tech-icons-text">{img_name}</div>
+    </Col>
+  ));
+}
+
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* Programming Languages */}
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiShell />
-      </Col>
-
-      {/* DataBases */}
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSqlite />
-      </Col>
-
-      {/* Frameworks  */}
-
-      <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFlask />
-      </Col>
-
-      {/* Extras */}
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+      {makeCol()}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,31 +1,28 @@
-import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiLinux,
-} from "react-icons/si";
+
+import postman from "../../Assets/TechIcons/postman.svg";
+import linux from "../../Assets/TechIcons/linux.svg";
+import vscode from "../../Assets/TechIcons/vscode.svg";
+
+function makeCol() {
+  const images_with_names = {
+    Linux : linux,
+    "VS Code": vscode,
+    Postman: postman,
+  };
+
+  return Object.entries(images_with_names).map(([img_name, image]) => (
+    <Col key={img_name} xs={4} md={2} className="tech-icons">
+      <img src={image} alt={img_name} height={30} />
+      <div className="tech-icons-text">{img_name}</div>
+    </Col>
+  ));
+}
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      {makeCol()}
     </Row>
   );
 }

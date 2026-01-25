97b36dbd0e81a611548ad569d016f3bd6630ebcf
Yash Aggarwal <vasu2013agg@gmail.com>
2026-01-25 19:50:51 +0530

    Updated Projects Page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,6 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
@@ -23,75 +20,43 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="SMTP Honeypot"
-              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              description="A honeypot server build in python using smptd library. Have features of creating a log file to help monitoring. Monitors port 22, 80, 21 and 25 "
-              ghLink="https://github.com/Ya-s-h/DemoHoneypot/blob/main/SMTPhonepot.py"
-              // demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Keylogger with detection"
-              // description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              description="A keylogger build in python using pynput library. Have features of sending the log file to the attacker's email using smtplib. Also have a feature of detecting the keylogger in the system."
-              ghLink="https://github.com/Ya-s-h/keylogger-with-detection"
-              // demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Twitch Watcher Discord bot"
-              // description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              description="A discord bot build in python discords library. Upon receiving, a specific message start watching the RLCS, to get the loot."
-              ghLink="https://github.com/Ya-s-h/twitch-watcher-discord-bot"
-              // demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col> */}
-
-          <Col md={4} className="project-card">
-            <ProjectCard
               imgPath={suicide}
-              isBlog={false}
+              hasLink={true}
               title="Remote Intensive Care application"
-              // description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
               description="A web application build using Flask, HTML, CSS and JavaScript. Helps in online appointment booking, delivering medicine and comes with a Heart Rate Sensor.
               Mainly Worked in the Backend Logic for this project"
               ghLink="https://github.com/TeamRocketBalleBalle/RICA-Remastered"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+              imgPath={chatify}
+              hasLink={true}
+              title="Riktionary"
+              description="A Java-based multiplayer game where players guess displayed images. 
+              The first player to guess correctly earns a point, and the first to reach 5 points wins the game."
+              ghLink="https://github.com/TeamRocketBalleBalle/Ricktionary"
             />
-          </Col> */}
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              hasLink={true}
+              title="Twitch Watcher Discord bot"
+              description="A Python-based Discord bot that listens for a specific command and starts a Docker container to monitor the RLCS stream and automatically claim available loot."
+              ghLink="https://github.com/Ya-s-h/twitch-watcher-discord-bot"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              hasLink={false}
+              title="Inventory Management System"
+              description="A MERN stack based inventory management system with multi-user authentication, hierarchical roles, CRUD activity logging, 
+              and automated low-stock email alerts using Nodemailer.
+              Delivered end-to-end as a client-focused freelancing project by a team of four."
+            />
+          </Col>
         </Row>
       </Container>
     </Container>

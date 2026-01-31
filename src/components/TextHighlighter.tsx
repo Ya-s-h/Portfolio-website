e134d18756689ce13f06f3ee546ffa7d2a76e375
Yash Aggarwal <vasu2013agg@gmail.com>
2026-01-31 21:29:07 +0530

    Refactor TextHighlighter to accept custom HTML component

--- src/components/TextHighlighter.tsx
+++ src/components/TextHighlighter.tsx
@@ -1,15 +1,18 @@
 import { Typography, useTheme } from "@mui/material";
 import React, { ReactNode } from "react";
-
 interface TextHighlighterProps {
+  htmlComponent?: React.ElementType;
   children: ReactNode;
 }
 
-function TextHighlighter({ children }: TextHighlighterProps) {
+function TextHighlighter({
+  htmlComponent = "span",
+  children,
+}: TextHighlighterProps) {
   const theme = useTheme();
   return (
     <Typography
-      component="span"
+      component={htmlComponent}
       sx={{
         color: theme.palette.secondary.main,
         fontWeight: 700,
